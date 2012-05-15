0b00dba77051c61ad4ea11aa81feb46a8ee943a3
Kuba <kubenstein@gmail.com>
2012-05-15 21:59:51 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wakemydyno::Application.config.secret_token = '0543f9568b203295bcbe3e7d416fc8cc51dee00833ea481d67ef7c419ac0346702c98ad9e1d6e71476aa574662f78b9b858f50c40cb5120ad1f4c6e28024fe14'
