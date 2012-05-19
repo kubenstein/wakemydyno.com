0a92b6a62fcd6fb156a1964aff58f4f0472b47bb
Kuba <kubenstein@gmail.com>
2012-05-19 16:05:59 +0200

    blinking dyno eyes

--- app/assets/javascripts/pings.js
+++ app/assets/javascripts/pings.js
@@ -1,2 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+jQuery(document).ready(function () {
+
+    function blink() {
+        function normal_eyes() {
+            $('#cute_dyno span.eyes').removeClass('eyes_blink');
+        }
+
+        function closed_eyes() {
+            $('#cute_dyno span.eyes').addClass('eyes_blink');
+        }
+
+        setTimeout(closed_eyes, 0);
+        setTimeout(normal_eyes, 100);
+        setTimeout(closed_eyes, 500)
+        setTimeout(normal_eyes, 600);
+    }
+
+    setInterval(blink, 9000);
+})
