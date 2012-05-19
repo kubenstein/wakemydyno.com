8868c55b91c7fc8e87d9b5de4ce644815b77a7b5
Kuba <kubenstein@gmail.com>
2012-05-20 00:06:48 +0200

    wakemydyno.txt file checking, need some timeout here

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,8 +1,11 @@
 class Url < ActiveRecord::Base
   attr_accessible :address
-  before_save :trim_ending_slash
+  before_validation :trim_ending_slash
 
   validates_format_of :address, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}\/?$/ix
+  validates_uniqueness_of :address
+  validate :wakefile_on_remote_server, :on => :create
+
 
 
   private
@@ -10,4 +13,12 @@ class Url < ActiveRecord::Base
   def trim_ending_slash
     address.chomp! '/'
   end
+
+  def wakefile_on_remote_server
+    if Net::HTTP.get_response(URI.parse("#{address}/wakemydyno.txt")).code.to_i != "200"
+      self.errors[:base] << "We can't find wakemydyno.txt file on requested site"
+      false
+    end
+  end
+
 end
