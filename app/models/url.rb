dec5a872b5ea5d3a0051517bcaebe0ac8d4b18a9
Kuba <kubenstein@gmail.com>
2012-05-20 13:59:38 +0200

    error msgs

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,8 +2,8 @@ class Url < ActiveRecord::Base
   attr_accessible :address
   before_validation :trim_ending_slash
 
-  validates_format_of :address, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}\/?$/ix
-  validates_uniqueness_of :address
+  validates_format_of :address, with: /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}\/?$/ix
+  validate :address_uniqueness
   validate :check_wakefile_existence, :on => :create
 
   #
@@ -24,6 +24,13 @@ class Url < ActiveRecord::Base
   def check_wakefile_existence
     if connect.code != "200"
       self.errors[:base] << "We can't find wakemydyno.txt file on requested site"
+      false
+    end
+  end
+
+  def address_uniqueness
+    if Url.where(address: self.address).count != 0
+      self.errors[:base] << "This url is already in our dyno database"
       false
     end
   end
