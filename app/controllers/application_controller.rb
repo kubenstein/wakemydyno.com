2106c95c66f20d9400de54f8db13c1d5dd22459e
Kuba <kubenstein@gmail.com>
2012-07-06 11:19:13 +0200

    redirect www -> non www

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :redirect_if_www
+
+  private
+
+  def redirect_if_www
+    request_uri = request.env['REQUEST_URI']
+    redirect_to request_uri.gsub('http://www.', 'http://') unless /http:\/\/www\./.match(request_uri).nil?
+  end
 end
