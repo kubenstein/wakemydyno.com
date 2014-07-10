4c908647ade7040c74af13576c8f26c03369ba00
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-07-10 19:20:56 +0200

    www-redirect gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :redirect_if_www
-
-  private
-
-  def redirect_if_www
-    request_uri = request.env['HTTP_HOST']
-    redirect_to request_uri.gsub('www.', 'http://') if request_uri.include?('www.')
-  end
 end
