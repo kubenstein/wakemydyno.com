d2875f1a1a52a2b0fd2237887dbcfa597017fd55
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-08-07 12:36:59 +0200

    BackgroundProcessSpawnerService specs

--- app/services/background_process_spawner_service.rb
+++ app/services/background_process_spawner_service.rb
@@ -1,25 +1,25 @@
 class BackgroundProcessSpawnerService
 
-  def self.spawn_ping_process
-    sleep 20.seconds
-    while true do
+  def self.spawn_ping_process(initial_sleep = 20.seconds, loop_condition = -> { true }, inloop_sleep = 20.minutes)
+    sleep initial_sleep
+    while loop_condition.call do
       puts "25min pinging..."
       PingService.new.perform
       puts "done."
-      sleep 25.minutes
+      sleep inloop_sleep
     end
   end
 
-  def self.spawn_url_check_process
-    initial_waiting_for_midnight_in_seconds = 24.hours - Time.now.seconds_since_midnight.to_i
+  def self.spawn_url_check_process(initial_sleep = 24.hours - Time.now.seconds_since_midnight.to_i, loop_condition = -> { true }, inloop_sleep = 24.hours)
+    initial_waiting_for_midnight_in_seconds = initial_sleep
     puts "initial waiting (#{initial_waiting_for_midnight_in_seconds}s) till next midnight... "
     sleep initial_waiting_for_midnight_in_seconds
 
-    while true do
+    while loop_condition.call do
       puts "daily url check..."
       UrlCheckService.new.perform
       puts "done."
-      sleep 24.hours
+      sleep inloop_sleep
     end
   end
 
--- config/initializers/field_error_silencer.rb
+++ config/initializers/field_error_silencer.rb
@@ -1,6 +1,4 @@
 #
 # disabling wrapping html form elements that yield validation erros with
 # <div class="field_with_errors">...</div>
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  html_tag
-end
+ActionView::Base.field_error_proc = -> (html_tag, instance) { html_tag }
