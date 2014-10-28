e77506ec4dd1a0a08e989c46ee0d30bbb5082287
Jakub Niewczas <niewczas.jakub@gmail.com>
2014-10-28 15:55:03 +0800

    add link to dynopoker github under /dynopoker

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Wakemydyno::Application.routes.draw do
 
   resources :urls, path: '/', only: [:create, :new]
   root :to => 'urls#new'
+  match '/dynopoker' => redirect('https://github.com/kubenstein/dynopoker')
 
 end
